8e12b35cfcf9c501cfb70dbfc01b0ac313519c72
eshwyane2k <100212094+eshwyane2k@users.noreply.github.com>
2022-03-17 15:48:18 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,17 @@
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character.
+ * @c: input value to check
+ * Return: int.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
