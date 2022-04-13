dfb2b0057bb832c4de8564859f4f1ae99f43de43
eshwyane2k <100212094+eshwyane2k@users.noreply.github.com>
2022-04-13 11:15:12 +0300

    Create 3-op_functions.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: add between a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return subtation between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return multiplication between a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return division between a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - entry point
+ * @a: get number 1
+ * @b: get number 2
+ * Return: return module between a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
